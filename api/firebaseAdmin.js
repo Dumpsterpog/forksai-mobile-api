37eb14d972cc46d9f77985f1daaf2d0aa17e15bc
pixelthreads <84399187+Dumpsterpog@users.noreply.github.com>
2025-12-11 17:30:42 +0530

    commit

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,9 +1,11 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
+
   admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN_KEY)),
-    storageBucket: "forksai.firebasestorage.app",
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "forksai.firebasestorage.app"
   });
 }
 
